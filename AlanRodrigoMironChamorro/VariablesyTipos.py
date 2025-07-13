c19110819b43b0821af85c0887d0a38128d5fe3c
alan rodrigo miron chamorro <alanmiron966@gmail.com>
2025-07-12 20:39:49 -0600

    VariablesyTipos

--- AlanRodrigoMironChamorro/VariablesyTipos.py
+++ AlanRodrigoMironChamorro/VariablesyTipos.py
@@ -1,4 +1,4 @@
-# Variables de números
+# Variables de numeros
 mi_entero = 10    # (int - entero)
 mi_flotante = 10.5  # (float - decimal)
 mi_largo = 1000000   # (int largo)
@@ -8,10 +8,10 @@ mi_corto = 3      # (int corto)
 short_text = "alan" # (str - texto)
 caracter = 'a'  # (str - carácter individual)
 
-# Print con tipo
-print("Entero (int):", mi_entero)
-print("Decimal (float):", mi_flotante)
-print("Entero largo (int largo):", mi_largo)
-print("Entero corto (int corto):", mi_corto)
-print("Texto (str):", short_text)
-print("Carácter (str):", caracter)
+# Print c
+print(" (int):", mi_entero)
+print(" (float):", mi_flotante)
+print(" (int largo):", mi_largo)
+print(" (int corto):", mi_corto)
+print("Texto :", short_text)
+print("Carácter :", caracter)
