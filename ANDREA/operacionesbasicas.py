f7c5d415aa57d9e6466fc78cabaf6222b37e38e7
andreher22 <160271676+andreher22@users.noreply.github.com>
2025-07-12 21:04:03 -0600

    operacionesbasicas

--- ANDREA/operacionesbasicas.py
+++ ANDREA/operacionesbasicas.py
@@ -4,6 +4,6 @@ b = 7
 
 
 print("La suma de a + b", a + b)
-print("La resta de a + b", a - b)
-print("La multiplicacion de a + b", a * b)
-print("La divicion de a + b", a / b)
+print("La resta de a - b", a - b)
+print("La multiplicacion de a * b", a * b)
+print("La divicion de a / b", a / b)
