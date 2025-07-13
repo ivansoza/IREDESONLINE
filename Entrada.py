fbdc638e9cf097c5e7c799a2a006e0d6afe9ca25
alan rodrigo miron chamorro <alanmiron966@gmail.com>
2025-07-12 20:46:29 -0600

    En

--- Entrada.py
+++ Entrada.py
@@ -1,6 +1,8 @@
 nombre = input("¿como te llamas? ")
 edad = input("¿Cuántos años tienes? ")
 correo = input("¿Cuál es tu correo electrónico? ")
+contacto = input("¿Cuál es tu número de contacto? ")    
+Direccion = input("¿Cuál es tu dirección? ")
 
 
 
@@ -8,3 +10,5 @@ correo = input("¿Cuál es tu correo electrónico? ")
 print("Tu Nombre:", nombre)
 print("Tu Edad:", edad)
 print("Correo:", correo)
+print("Contacto:", contacto)
+print("Dirección:", Direccion)  
