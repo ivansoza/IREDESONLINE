fbf75cf016d266968717ede0dc8a1fa45989310b
Alexanderpastranaguzman <alexpasguzman@gmai.com>
2025-07-12 20:30:27 -0600

    OperacionesBasicas

---
+++ AlexanderPastranaGuzman/OperacionesBasicas.py
@@ -0,0 +1,8 @@
+
+a = 8
+b = 3
+
+print("El resultado de la suma de a + b es ", a+b)
+print("El resultado de la multiplicacion de a * b es ", a*b)
+print("El resultado de la resta de a - b es ", a-b)
+print("El resultado de la divicion de a / b es ", a/b)
