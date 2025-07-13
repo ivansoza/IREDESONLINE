8901a1a63c5647186c6793f4941e520a559ee9de
alan rodrigo miron chamorro <alanmiron966@gmail.com>
2025-07-12 20:43:53 -0600

    entrada

---
+++ Entrada.py
@@ -0,0 +1,10 @@
+nombre = input("¿como te llamas? ")
+edad = input("¿Cuántos años tienes? ")
+correo = input("¿Cuál es tu correo electrónico? ")
+
+
+
+# Imprimir
+print("Tu Nombre:", nombre)
+print("Tu Edad:", edad)
+print("Correo:", correo)
