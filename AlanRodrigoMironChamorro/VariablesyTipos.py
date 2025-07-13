db307170c21219b09622f93640a9d95443f7045c
alan rodrigo miron chamorro <alanmiron966@gmail.com>
2025-07-12 20:29:23 -0600

    VariablesyTipos

---
+++ AlanRodrigoMironChamorro/VariablesyTipos.py
@@ -0,0 +1,13 @@
+"variabes de numeros"
+
+int= 10 (enteros)
+flotante = 10.5 (decimales)
+long = 1000000 (numeros mayores a 32 bits)
+short = 3 (enteros pequeños)
+
+
+"variables de texto"
+
+short_text = "alan" (de texto)
+char = 'a' (un caracter)   
+ 
