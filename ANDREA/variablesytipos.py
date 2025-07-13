b6ccd9630e967f9f4021f2bd0a3042fe3f319f5d
andreher22 <160271676+andreher22@users.noreply.github.com>
2025-07-12 20:30:03 -0600

    variables y tipos

---
+++ ANDREA/variablesytipos.py
@@ -0,0 +1,13 @@
+
+
+
+entero = 10
+flotante = 19.2
+cadena = "andrea robles hernandez"
+booleano = True
+
+print(entero, type(entero))
+
+print(flotante, type(flotante))
+print(cadena, type(cadena))
+print(booleano, type(booleano))
