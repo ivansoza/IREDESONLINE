7bd254596fc4882fc31a7e9249696417a29d65e1
andreher22 <160271676+andreher22@users.noreply.github.com>
2025-07-12 20:43:16 -0600

    entradasusuarios

---
+++ ANDREA/entradasusuario.py
@@ -0,0 +1,13 @@
+
+nombre_alumno = input("¿Cual es tu nombre?")
+edad = input("¿Cuantos años tienes?")
+
+email = input("¿Cual es tu correo?")
+nuemro_telefonico = input("¿Cual es tu numero de contacto?")
+direccion = input("¿Donde vives?")
+
+print("Hola", nombre_alumno)
+print("21", edad)
+print("ani@gmail.com", email)
+print("5555555555", nuemro_telefonico)
+print("Mexico", direccion)
