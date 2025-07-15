3c55c3239ef04487989b99502790e7536b9d1ab8
G_A_Z_Z <zavalaamisael@gmail.com>
2025-07-15 15:53:41 -0600

    Programa actualizado

--- Gaston_Amisael/Ejercicio3.py
+++ Gaston_Amisael/Ejercicio3.py
@@ -1,4 +1,13 @@
+#SOLUCIÓN AL RETO
+
 nombre = input("Hola¡¡  ¿Cual es tu nombre?")
 edad = input("¿Cual es tu edad?")
 
 print("Hola mi nombre es" + nombre + " y tengo" + edad + "años de edad")
+
+
+# EJERCICIO QUE PUSO EN CLASE
+#nombre_alumno = input("como te llamas?")
+#edad = input("cual es tu edad?")
+#print("nombre", nombre_alumno)
+#print("edad:", edad)
