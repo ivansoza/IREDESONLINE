aa0672cedb7dab675327493619ddf6bc48e295e7
G_A_Z_Z <zavalaamisael@gmail.com>
2025-07-15 16:05:05 -0600

    Se agrego el ejercicio 5

---
+++ Gaston_Amisael/Ejercicio5.py
@@ -0,0 +1,18 @@
+#SOLUCIÓN AL RETO
+
+entero = 5
+flotante = 9.6338
+cadena = "Primeros pasos en github"
+booleano = False
+
+print(f"Los valores de las variables junatas son: Entero={entero}, Flotante={flotante}, Cadena='{cadena}', Booleano={booleano}")
+
+#EJERCICIO QUE PUSO EN CLASE
+#entero = 10
+#flotante = 19.2
+#cadena = "arley ivan solis zacapantzi"
+#booleano = True
+#print(entero, type(entero))
+#print(flotante, type(flotante))
+#print(cadena, type(cadena))
+#print(booleano, type(booleano))
