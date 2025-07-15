be3f2844aec8e6e46d4061ea33703b2f846f5ffb
G_A_Z_Z <zavalaamisael@gmail.com>
2025-07-15 17:32:50 -0600

    Se agrego programa 4

---
+++ Gaston_Amisael/Ejercicio4.py
@@ -0,0 +1,20 @@
+ejercicio = input("Elige una operación(*,/,+,-):" )
+
+a = float(input("Numero 1:"))
+b = float(input("Numero 2:"))
+
+if ejercicio == "*":
+    print("El resultado es:", a * b)
+elif ejercicio == "/":
+    if b !=0:
+        print("El resultado es:", a / b)
+    else:
+        print("No se puede dividir entre cero")
+elif ejercicio == "+":
+    print("El resultado es:", a + b)
+elif ejercicio == "-":
+    print("El resultado es:", a - b)
+    
+
+
+        
