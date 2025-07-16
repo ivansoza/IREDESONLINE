33ef4a7f69f3feb4ae91104620cabc15e151a9b9
Alexanderpastranaguzman <alexpasguzman@gmai.com>
2025-07-15 21:26:40 -0600

    3.py

---
+++ AlexanderPastranaGuzman/Evaluacion/3.py
@@ -0,0 +1,10 @@
+
+a = int(input("Numero A: "))
+b = int(input("Número B: "))
+
+if a > b:
+    print("A es mayor")
+elif a < b:
+    print("B es mayor")
+else:
+    print("Los numeros son iguales")
