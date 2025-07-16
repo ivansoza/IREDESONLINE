f2aea699faf455cde39e06c70623470c8a4acde0
G_A_Z_Z <zavalaamisael@gmail.com>
2025-07-15 21:33:02 -0600

    Ejercicio 4 hecho y subido

---
+++ Examen/Ejercicio4.py
@@ -0,0 +1,12 @@
+nota = float(input("Ingresa a calificaión:"))
+
+if nota  > 8:
+    print ("aprobado")
+elif nota >= 6:
+    print ("regular")
+elif nota >= 4:
+    print ("recursamiento")
+else:
+    print ("reprobado")
+    
+#poner más opciones como regular
