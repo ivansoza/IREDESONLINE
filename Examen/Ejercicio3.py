b647861e99d9825779dce52ecf989a418846a373
G_A_Z_Z <zavalaamisael@gmail.com>
2025-07-15 21:31:21 -0600

    Ejercicio 3 hecho y subido

---
+++ Examen/Ejercicio3.py
@@ -0,0 +1,13 @@
+a = int(input("numero a:")) #6
+b = int(input("numero b:")) #3
+
+if a > b:
+    print("a es mayor que b")
+    
+elif a == b:
+    print("a es igual a b")
+
+else:
+    print("b es mayor o igual que a")
+    
+    #RETO PONER MENSAJE SI SON IGUALES 
